b675e9971105f3b4dbf69f0dd22926e73d0c378d
Natallia <natasyo23@gmail.com>
2026-04-01 12:31:25 +0300

    feat:add user

---
+++ src/modules/users/dto/create-user.dto.ts
@@ -0,0 +1,22 @@
+import {
+  IsEnum,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { UserRole } from './UserRole';
+export class CreateUserDto {
+  @IsString()
+  @MinLength(3, { message: 'Login must be at least 3 characters' })
+  login: string;
+
+  @IsString()
+  @MinLength(3, { message: 'Password must be at least 3 characters' })
+  @MaxLength(30, { message: 'Password must be at least 30 characters' })
+  password: string;
+
+  @IsOptional()
+  @IsEnum(UserRole, { message: 'Role will be admin, editor, viewer' })
+  role?: 'admin' | 'editor' | 'viewer';
+}
