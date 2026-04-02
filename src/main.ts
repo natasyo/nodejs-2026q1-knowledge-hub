9bdd9c5c2a753cae8adfde1e439a68f155b00187
Natallia <natasyo23@gmail.com>
2026-04-02 22:27:21 +0300

    feat:swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Documentation')
+    .setDescription('Documentation')
+    .build();
+  const doc = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, doc);
   await app.listen(4000);
 }
 bootstrap();
