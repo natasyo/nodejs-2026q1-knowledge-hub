48cc39c7e658241e149b98cc5155bb26475a131b
Natallia <natasyo23@gmail.com>
2026-04-17 17:03:37 +0300

    feat:  satrt:dev command

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { CategoriesModule } from './modules/categories/categories.module';
 import { CommentsModule } from './modules/comments/comments.module';
 
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { PrismaModule } from './modules/prisma/prisma.module';
     CategoriesModule,
     CommentsModule,
     PrismaModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
